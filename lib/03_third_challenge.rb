4f0de6cbe937baf7002b8d269506401745f40287
abstract-inheritance-c040 <doe.mahamud1@gmail.com>
2021-05-30 00:19:58 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -1,10 +1,9 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {
+      :patriarch => {name: "Lord Montague", age: "53"}
         #your key/value pairs here
-        },
-      :matriarch => {
+      :matriarch => {name: "Lady Montague", age: "54"
         #your key/value pairs here
         },
       :hero => {
